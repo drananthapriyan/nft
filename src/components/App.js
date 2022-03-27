bae93b7d001ddbf60b8edd423b94ee859878aab4
drananthapriyan <52826082+drananthapriyan@users.noreply.github.com>
2022-03-27 11:16:05 +0400

    upload image with base64

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,51 @@
-import React, { Component } from 'react';
-import Web3 from 'web3'
-import './App.css';
-import Color from '../abis/Color.json'
+import React, { Component } from "react";
+import Web3 from "web3";
+import "./App.css";
+import Color from "../abis/Color.json";
 
 class App extends Component {
-
   async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+    await this.loadWeb3();
+    await this.loadBlockchainData();
   }
 
   async loadWeb3() {
     if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
     }
   }
 
   async loadBlockchainData() {
-    const web3 = window.web3
+    const web3 = window.web3;
     // Load account
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
 
-    const networkId = await web3.eth.net.getId()
-    const networkData = Color.networks[networkId]
-    if(networkData) {
-      const abi = Color.abi
-      const address = networkData.address
-      const contract = new web3.eth.Contract(abi, address)
-      this.setState({ contract })
-      const totalSupply = await contract.methods.totalSupply().call()
-      this.setState({ totalSupply })
+    const networkId = await web3.eth.net.getId();
+    const networkData = Color.networks[networkId];
+    if (networkData) {
+      const abi = Color.abi;
+      const address = networkData.address;
+      const contract = new web3.eth.Contract(abi, address);
+      this.setState({ contract });
+      const totalSupply = await contract.methods.totalSupply().call();
+      this.setState({ totalSupply });
       // Load Colors
       for (var i = 1; i <= totalSupply; i++) {
-        const color = await contract.methods.colors(i - 1).call()
+        const color = await contract.methods.colors(i - 1).call();
         this.setState({
-          colors: [...this.state.colors, color]
-        })
+          colors: [...this.state.colors, color],
+        });
       }
     } else {
-      window.alert('Smart contract not deployed to detected network.')
+      window.alert("Smart contract not deployed to detected network.");
     }
   }
 
@@ -57,16 +56,49 @@ class App extends Component {
         colors: [...this.state.colors, color]
       })
     })
+    console.log(color);
+    console.log(this.state.account);
   }
 
+  toDataURL(src, callback, outputFormat) {
+    var img = new Image();
+    img.crossOrigin = "Anonymous";
+    img.onload = function() {
+      var canvas = document.createElement("CANVAS");
+      var ctx = canvas.getContext("2d");
+      var dataURL;
+      canvas.height = this.naturalHeight;
+      canvas.width = this.naturalWidth;
+      ctx.drawImage(this, 0, 0);
+      dataURL = canvas.toDataURL(outputFormat);
+      callback(dataURL);
+    };
+    img.src = src;
+    if (img.complete || img.complete === undefined) {
+      img.src =
+        "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+      img.src = src;
+    }
+  }
+
+  onFileChange = (event) => {
+    this.toDataURL(URL.createObjectURL(event.target.files[0]), function(
+      dataUrl
+    ) {
+      //document.getElementById("dataurl").innerHTML = dataUrl;
+      document.getElementById("imgElem").setAttribute("src", dataUrl);
+      document.getElementById("dataUrl").value = dataUrl;
+    });
+  };
+
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      account: '',
+      account: "",
       contract: null,
       totalSupply: 0,
-      colors: []
-    }
+      colors: [],
+    };
   }
 
   render() {
@@ -79,11 +111,13 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Color Tokens
+            NFT Marketplace
           </a>
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-white"><span id="account">{this.state.account}</span></small>
+              <small className="text-white">
+                <span id="account">{this.state.account}</span>
+              </small>
             </li>
           </ul>
         </nav>
@@ -92,35 +126,51 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <h1>Issue Token</h1>
-                <form onSubmit={(event) => {
-                  event.preventDefault()
-                  const color = this.color.value
-                  this.mint(color)
-                }}>
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault();
+                    const color = this.color.value;
+                    this.mint(color);
+                  }}
+                >
                   <input
-                    type='text'
-                    className='form-control mb-1'
-                    placeholder='e.g. #FFFFFF'
-                    ref={(input) => { this.color = input }}
+                    type="file"
+                    className="form-control mb-1"
+                    onChange={this.onFileChange}
                   />
                   <input
-                    type='submit'
-                    className='btn btn-block btn-primary'
-                    value='MINT'
+                    type="hidden"
+                    className="form-control mb-1"
+                    placeholder="e.g. #FFFFFF"
+                    id="dataUrl"
+                    ref={(input) => {
+                      this.color = input;
+                    }}
+                  />
+                  <input
+                    type="submit"
+                    className="btn btn-block btn-primary"
+                    value="MINT"
                   />
                 </form>
+
+                <img id="imgElem" alt="imgElem"></img>
               </div>
             </main>
           </div>
-          <hr/>
+          <hr />
           <div className="row text-center">
-            { this.state.colors.map((color, key) => {
-              return(
+            {this.state.colors.map((color, key) => {
+              return (
                 <div key={key} className="col-md-3 mb-3">
-                  <div className="token" style={{ backgroundColor: color }}></div>
-                  <div>{color}</div>
+                  <img  src={color} alt="img"></img>
+                  {/* <div
+                    className="token"
+                    style={{ backgroundColor: color }}
+                  ></div> */}
+                  {/* <div>{color}</div> */}
                 </div>
-              )
+              );
             })}
           </div>
         </div>
